da741e8aca8836ad2d050e5cdff3287e3aa5223f
Nathaniel Torres <nasanierunatto@gmail.com>
2023-07-02 09:52:38 +0800

    Deployment fixes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB()
 const app = express()
 
 app.use(cors({
-  origin: [""],
+  origin: ["https://test-mern-app-books-frontend.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
