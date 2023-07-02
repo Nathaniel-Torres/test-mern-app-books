bb090414341055f7583c5aa6b633c39ffa4ff3e0
Nathaniel Torres <nasanierunatto@gmail.com>
2023-07-02 08:51:10 +0800

    update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
@@ -6,18 +7,24 @@ const customBookRoutes = require('./routes/BookRoutes')
 const customUserRoutes = require('./routes/UserRoutes')
 const { errorHandler } = require('./middleware/ErrorMiddleware')
 const connectDB = require('./config/Database')
-const cors = require('cors')
+// const cors = require('cors')
 
 connectDB()
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/books', customBookRoutes)
 app.use('/api/users', customUserRoutes)
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}
 
 app.use(errorHandler)
 
