3d52ce5bdc8b5a02f5dfa227939fbd24f507c277
Nathaniel-Torres <nasanierunatto@gmail.com>
2023-07-03 07:22:16 +0800

    Navbar redesign

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,14 @@
 import { Link, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { logout, reset } from '../features/auth/AuthSlice'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
-  const [isShowed, setShowed] = useState('')
-
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const { user } = useSelector((state) => state.auth)
-
-  const onClick = (e) => {
-    isShowed === '' ? setShowed('show') : setShowed('')
-  }
 
   const onLogout = () => {
     dispatch(logout())
@@ -21,66 +17,34 @@ function Header() {
   }
 
   return (
-    <nav className='navbar navbar-expand-md border-bottom'>
-      <div className='container'>
-
-        <div className='logo'>
-          <Link to='/' className='navbar-brand'>Book Record</Link>
-        </div>
-
-        <button
-          className='navbar-toggler'
-          type='buton'
-          data-bs-toggle='collapse'
-          data-bs-target='#to-collapse'
-          aria-expanded='false'
-          onClick={onClick}
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-
-        <div className={'navbar-collapse collapse' + isShowed} id='to-collapse'>
-          <ul className='navbar-nav ms-auto'>
-
-            {user ? (
-              <li className='nav-item'>
-                <button className='btn btn-danger' onClick={onLogout}>
-                  <i className='bi bi-person'></i> Logout
-                </button>
-              </li>
-            ) : (
-              <>
-                <li className='nav-item'>
-                  <Link to='/login' className='nav-link' onClick={onClick}>
-                    <i className='bi bi-person'></i> Login
-                  </Link>
-                </li>
-
-                <li className='nav-item'>
-                  <Link to='/register' className='nav-link' onClick={onClick}>
-                  <i className='bi bi-person'></i> Register
-                </Link>
-                </li>
-              </>
-            )}
-
-            {/* <li className='nav-item'>
-              <Link to='/login' className='nav-link' onClick={onClick}>
-                <i className='bi bi-person'></i> Login
-              </Link>
-            </li>
-
-            <li className='nav-item'>
-              <Link to='/register' className='nav-link' onClick={onClick}>
-              <i className='bi bi-person'></i> Register
-            </Link>
-            </li> */}
-
-          </ul>
-        </div>
-
-      </div>
-    </nav>
+      <Navbar expand="md" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href='/'>Books.io</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-collapsible" />
+          <Navbar.Collapse id="navbar-collapsible">
+            <Nav className="ms-auto">
+              
+              {user ? (
+                <Nav.Link onClick={onLogout}>
+                  <i className='bi bi-arrow-right'></i>
+                  Logout
+                </Nav.Link>
+              ) : (
+                <>
+                  <Nav.Link href="/login">
+                    <i className="bi bi-arrow-left"></i>
+                    Login
+                  </Nav.Link>
+                  <Nav.Link href="/register">
+                    <i className="bi bi-person"></i>
+                    Register
+                  </Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
   )
 }
 
