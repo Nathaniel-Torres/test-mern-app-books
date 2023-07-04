971a9053bd0c4a3c18680f93f2ddbdce2ca8a9c5
Nathaniel-Torres <nasanierunatto@gmail.com>
2023-07-04 11:58:08 +0800

    Register page redesign

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,7 +4,12 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { register, reset } from '../features/auth/AuthSlice'
 import Spinner from '../components/Spinner'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -61,68 +66,50 @@ function Register() {
   }
 
   return (
-    <Row className='h-100'>
-      <div className='container'>
-        <h1>Create an account</h1>
-        <h5>Please fill in all the required fields.</h5>
-
-        <form onSubmit={onSubmit}>
-
-          <div className="form-group mb-2">
-            <input
-              className='form-control'
-              type="text"
-              name="name"
-              id="name"
-              placeholder='Enter your name'
-              onChange={onChange} 
-              data-bs-theme='dark'
-            />
-          </div>
-
-          <div className="form-group mb-2">
-            <input
-              className='form-control'
-              type="email"
-              name="email"
-              id="email"
-              placeholder='Enter your email'
-              onChange={onChange} 
-              data-bs-theme='dark'
-            />
-          </div>
-
-          <div className="form-group mb-2">
-            <input
-              className='form-control'
-              type="password"
-              name="password"
-              id="password"
-              placeholder='Enter your new password'
-              onChange={onChange} 
-              data-bs-theme='dark'
-            />
-          </div>
-
-          <div className="form-group mb-4">
-            <input
-              className='form-control'
-              type="password"
-              name="cpassword"
-              id="cpassword"
-              placeholder='Confirm your new password'
-              onChange={onChange} 
-              data-bs-theme='dark'
-            />
-          </div>
-
-          <div className="form-group">
-            <button className='btn btn-dark'>Submit</button>
-          </div>
-
-        </form>
-      </div>
-    </Row>
+    <Container className='h-100'>
+      <Row className='mb-3 justify-content-center'>
+        <Col md='6'>
+          <Container className='bg-light rounded-3 py-4 mb-4'>
+            <Form onSubmit={onSubmit}>
+              <Row className='text-center mb-2'>
+                <i className='login-logo bi bi-box-arrow-in-right cursor-pointer'></i>
+                <h1 className='fw-bold'>Create an account</h1>
+                <p>To start using this app, create an account if you do not have one.</p>
+              </Row>
+              <Container className='mb-3'>
+                <InputGroup>
+                  <InputGroup.Text id='name-label' className='bg-light'><i className='bi bi-person'></i></InputGroup.Text>
+                  <Form.Control aria-describedby="name-label" type='text' name='name' id='name' placeholder='Enter your name' onChange={onChange} />
+                </InputGroup>
+              </Container>
+              <Container className='mb-3'>
+                <InputGroup>
+                  <InputGroup.Text id='email-label' className='bg-light'><i className='bi bi-envelope-fill'></i></InputGroup.Text>
+                  <Form.Control aria-describedby="email-label" type='email' name='email' id='email' placeholder='Enter your email' onChange={onChange} />
+                </InputGroup>
+              </Container>
+              <Container className='mb-3'>
+                <InputGroup>
+                  <InputGroup.Text id='password-label' className='bg-light'><i className='bi bi-lock'></i></InputGroup.Text>
+                  <Form.Control aria-describedby="password-label" type='password' name='password' id='password' placeholder='Enter your new password' onChange={onChange} />
+                </InputGroup>
+              </Container>
+              <Container className='mb-3'>
+                <InputGroup>
+                  <InputGroup.Text id='cpassword-label' className='bg-light'><i className='bi bi-lock-fill'></i></InputGroup.Text>
+                  <Form.Control aria-describedby="cpassword-label" type='password' name='cpassword' id='password' placeholder='Confirm your new password' onChange={onChange} />
+                </InputGroup>
+              </Container>
+              <Form.Group className='mb-3'>
+                <Container className='d-flex justify-content-center align-items-center'>
+                  <Button type='submit' className='w-100' variant='dark'>Submit</Button>
+                </Container>
+              </Form.Group>
+            </Form>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
