b3b1176732af41ff488c63fcb4fc8d845ff1ecfa
Nathaniel-Torres <nasanierunatto@gmail.com>
2023-07-04 15:42:28 +0800

    UI update rollback

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,15 @@
 import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-//import BookForm from '../components/BookForm'
+import BookForm from '../components/BookForm'
 import BookItem from '../components/BookItem'
 import Spinner from '../components/Spinner'
 import { displayBooks, reset } from '../features/books/BookSlice'
 import { toast } from 'react-toastify'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
-import Button from 'react-bootstrap/Button'
 
 function Dashboard() {
-  const reactSwal = withReactContent(Swal)
 
   const navigate = useNavigate()
   const dispatch = useDispatch()
@@ -55,80 +51,6 @@ function Dashboard() {
     })
   }
 
-  // try
-
-  const [bookData, setBookData] = useState({
-    title: '',
-    desc: '',
-    price: '',
-  })
-
-  const onSubmit = e => {
-    e.preventDefault()
-
-    dispatch(addBook(bookData))
-  }
-
-  const onChange = (e) => {
-    setBookData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }))
-  }
-
-  const BookForm = (
-    <div className='container bg-secondary rounded py-3'>
-      <form onSubmit={onSubmit}>
-
-        <h3 className="text-light mb-3">Add New Book</h3>
-        <p className="text-light mb-4">Fill in the fields to add a book.</p>
-
-        <div className="form-group mb-2">
-          <input
-            className='form-control'
-            type="text"
-            name="title"
-            id="title"
-            placeholder='Book title'
-            onChange={onChange} 
-            value={bookData.title}
-          />
-        </div>
-
-        <div className="form-group mb-2">
-          <input
-            className='form-control'
-            type="text"
-            name="desc"
-            id="desc"
-            placeholder='Book Description'
-            onChange={onChange} 
-            value={bookData.desc}
-          />
-        </div>
-
-        <div className="form-group mb-2">
-          <input
-            className='form-control'
-            type="number"
-            name="price"
-            id="price"
-            placeholder='Book Price'
-            onChange={onChange} 
-            value={bookData.price}
-          />
-        </div>
-
-        <div className="form-group">
-          <button type='submit' className='btn btn-dark'>Submit</button>
-        </div>
-
-      </form>
-    </div>
-  )
-
-  // try
-
   return (
     <Container className='h-100'>
       <Container className='d-flex gap-2 mb-2'>
@@ -147,9 +69,6 @@ function Dashboard() {
             <h6>You have no books in your library.</h6>
           </Row>
         )}
-      </Container>
-      <Container className='my-3'>
-        <Button type='button' onClick={SwalFire} variant='secondary' className='text-light'>Test</Button>
       </Container>
       <BookForm />
     </Container>
