63fb5a75b1d47fb43f3e115dd4b5979ad1b083f9
Nathaniel-Torres <nasanierunatto@gmail.com>
2023-07-05 20:07:16 +0800

    Added functional links on footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,10 +5,15 @@ import Col from 'react-bootstrap/Col'
 function Footer() {
   return (
     <Container fluid className="footer bg-dark text-light py-3">
-      <Row className='mb-3'>
+      <Row className='mb-5'>
         <Container className='text-center'>
           <h6>Nathaniel Torres</h6>
           <p>@ 2023</p>
+          <p><i className='bi bi-envelope'></i>&nbsp;:&nbsp;it.nathaniel.m.torres@gmail.com</p>
+          <a href='https://nathaniel-torres.github.io/' className='text-light'>
+            <i className='bi bi-globe-americas'></i>&nbsp;:&nbsp;
+            {('https://nathaniel-torres.github.io/').toString()}
+          </a>
         </Container>
       </Row>
       <hr />
@@ -17,11 +22,31 @@ function Footer() {
           <Container className='text-center'>
             <p>built using:</p>
             <Row className='align-items-center justify-content-center'>
-              <Col xs='2'>reactjs logo</Col>
-              <Col xs='2'>reduxjs logo</Col>
-              <Col xs='2'>expressjs logo</Col>
-              <Col xs='2'>nodejs logo</Col>
-              <Col xs='2'>mongodb logo</Col>
+              <Col xs='2'>
+                <a href='https://react.dev/' className='text-light'>
+                  React<i className='bi bi-box-arrow-in-up-right'></i>
+                </a>
+              </Col>
+              <Col xs='2'>
+                <a href='https://redux.js.org/' className='text-light'>
+                  Redux<i className='bi bi-box-arrow-in-up-right'></i>
+                </a>
+              </Col>
+              <Col xs='2'>
+                <a href='https://expressjs.com/' className='text-light'>
+                  Express<i className='bi bi-box-arrow-in-up-right'></i>
+                </a>
+              </Col>
+              <Col xs='2'>
+                <a href='https://nodejs.org/en' className='text-light'>
+                  Node<i className='bi bi-box-arrow-in-up-right'></i>
+                </a>
+              </Col>
+              <Col xs='2'>
+                <a href='https://www.mongodb.com/' className='text-light'>
+                  MongoDB<i className='bi bi-box-arrow-in-up-right'></i>
+                </a>
+              </Col>
             </Row>
           </Container>
         </Col>
@@ -29,9 +54,10 @@ function Footer() {
       <Row className='mt-3 mb-3 justify-content-center'>
         <Col xs lg='6'>
           <Container className='d-flex gap-2 justify-content-center'>
-            <p>hosted at:</p>
-            <i className='bi bi-github'></i>
-            <i>Vercel logo</i>
+            <p>GitHub project repository:</p>
+            <a href='https://github.com/Nathaniel-Torres/test-mern-app-books' className='text-light'>
+              <i className='bi bi-github'></i>&nbsp;&nbsp;here
+            </a>
           </Container>
         </Col>
       </Row>
